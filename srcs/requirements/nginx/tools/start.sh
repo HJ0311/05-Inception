ee7e9813c7b55c04d4f61757cbc480ae329432ef
HJ0311 <giladoja8311@gmail.com>
2025-01-03 13:25:23 +0900

    mody: docker-compose.yml

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export ${cat ../../../.env | xargs}
+
 # self-signed certification create
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 -keyout /etc/ssl/private/nginx-selfsigned.key \
