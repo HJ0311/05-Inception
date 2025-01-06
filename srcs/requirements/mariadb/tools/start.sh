83ad1ccc670c1847f334bc1852aac66afdbbf67b
HJ0311 <giladoja8311@gmail.com>
2025-01-06 17:30:09 +0900

    Inception

---
+++ srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mysql start
+
+mysqld_safe --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
+
+sleep 10
+
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" > db1.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ROOT_USER}'@'%';" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
