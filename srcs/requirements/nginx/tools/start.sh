291020698e0129b52e042e4a3371518c0328e298
HJ0311 <giladoja8311@gmail.com>
2025-01-13 15:13:57 +0900

    FEAT: wordpress start.sh

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -20,13 +20,14 @@ server {
 
 	root /var/www/html;
 	server_name ${DOMAIN_NAME};
+	index index.php index.html index.htm;
 	
 	location ~ \.php$ {
 		include snippets/fastcgi-php.conf;
 		fastcgi_pass wordpress:9000;
     }
 }
-" > /etc/nginx/sites-available/default
+" >> /etc/nginx/sites-available/default
 
 # 심볼릭 링크 생성
 if [ ! -L /etc/nginx/sites-enabled/default ]; then
