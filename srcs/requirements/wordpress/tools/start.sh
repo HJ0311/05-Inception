561314b1d2f25c890becbcdb5cd3843291531b66
HJ0311 <giladoja8311@gmail.com>
2025-01-15 14:51:11 +0900

    FEAT: wordpress start.sh

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-sleep 6
+mkdir -p /var/www
+mkdir -p /var/www/html
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+cd /var/www/html
 
-chmod +x wp-cli.phar
+rm -rf *
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+chmod +x wp-cli.phar 
 
 mv wp-cli.phar /usr/local/bin/wp
 
-mkdir -p /run/php
+wp core download --allow-root
 
-# 루트 권한 실행
-cd /var/www/html && wp core download --allow-root
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-mv wp-config-sample.php wp-config.php
+mv /wp-config.php /var/www/html/wp-config.php
 
-wp config set SERVER_PORT 3306 --allow-root
+sed -i -r "s/db/$MYSQL_DATABASE/1"   wp-config.php
+sed -i -r "s/user/$MYSQL_USER/1"  wp-config.php
+sed -i -r "s/pwd/$MYSQL_PASSWORD/1"    wp-config.php
 
-wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
-wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html
-wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
-wp config set DB_HOST $DB_HOST --allow-root --path=/var/www/html
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
-wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --allow-root --path=/var/www/html
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 
-wp user create $WP_USER --role=author --user_pass=$WP_USER_PASSWORD --allow-root --path=/var/www/html
+wp theme install astra --activate --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+wp plugin update --all --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir /run/php
+
+
+
+wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.4 -F
---
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,26 @@
+<?php
+
+define( 'DB_NAME', 'db' );
+
+define( 'DB_USER', 'user' );
+
+define( 'DB_PASSWORD', 'pwd' );
+
+define( 'DB_HOST', 'mariadb' );
+
+define( 'DB_CHARSET', 'utf8' );
+
+define( 'DB_COLLATE', '' );
+
+define ( 'WP_ALLOW_REPAIR', true);
+
+define('AUTH_KEY',         '@N={5hY_d[hkx%}RGK(N.+VX{n;@:_|Yw~-l0_BxES}b!K|fbz!T4VF4-<wl!wnI');
+define('SECURE_AUTH_KEY',  '9PL-3Tiv7,v+emSbNER*m=XGcs=i!dO]o4#_^<X)uw|_ys9&(@vPQTnNq|h$|+ev');
+define('LOGGED_IN_KEY',    '8Wx4^.saF;W]UI#q_<6cV$(09zeuZv</AoS`R:ZL({rt~ew+WMRZ>uCF@OGrY#4p');
+define('NONCE_KEY',        'Qjih{C[0J(yDDS`24%QG0pK0xb*tQhI#sA#dt5=PX(42:#z<-B@@QEE|-, {Uko&');
+define('AUTH_SALT',        'icD!qT,g|E&WEW{b-8IrLI:K12Z6t*J&34L:Qr2>pfL<P(1(n|?5.39#S_lo@GUZ');
+define('SECURE_AUTH_SALT', '<-6ftBPOFF.Q-Tq|>ve+)76-.o+onwxc8D2jR4DJ9xX?O}#|mV,FoVssxF>M!R3S');
+define('LOGGED_IN_SALT',   '6DbE7c!E(u B`eu|aMHg%H`$e++r?*hR56#Vuq.Bk{y-(n49z-:6AjMQ$|tm4YV(');
+define('NONCE_SALT',       'eVXLZffe@?Wj.exa@::dkT;nHu,j{.j_0tI#P6x0c1|;,knE;+PVnyg]Te~2.`os');
+
+?>
